1ff75098725c6755bef9affbaa5eb77e13d5b173
riccisi <simone.ricciardi@gmail.com>
2018-03-12 16:57:54 +0100

    - pdd test

--- infrastructure/src/main/java/it/tasgroup/xtderp/xtdplatform/infrastructure/media/json/JsonMedia.java
+++ infrastructure/src/main/java/it/tasgroup/xtderp/xtdplatform/infrastructure/media/json/JsonMedia.java
@@ -1,10 +1,11 @@
 package it.tasgroup.xtderp.xtdplatform.infrastructure.media.json;
 
 import it.tasgroup.xtderp.xtdplatform.infrastructure.media.Media;
-import it.tasgroup.xtderp.xtdplatform.infrastructure.media.RenderedList;
-import it.tasgroup.xtderp.xtdplatform.infrastructure.media.RenderedObject;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * @todo make a tesr fot this class
+ */
 @RequiredArgsConstructor
 public class JsonMedia implements Media<JsonRendered> {
 
